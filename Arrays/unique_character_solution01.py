e656971f31a37d213bd8b16f8ec66b2727acad6f
Ravi Boodher <raviboodher@users.noreply.github.com>
2018-06-06 09:10:06 +0530

    Unique character problem solved

---
+++ Arrays/unique_character_solution01.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+def unique_char(s):
+    """ (str) --> bool
+
+    Determine if the string contains unique characters. If there are duplicate characters return False.
+
+    Example: 'abcde' is True since it has all unique chars.
+             'aabcde' is False since character 'a' appears two times in the string.
+
+    >>> unique_char('abcde')
+    True
+    >>> unique_char('aabcde')
+    False
+    >>> unique_char('abCc')
+    True
+    """
+
+    for char in s:
+        if s.count(char) > 1:
+            return False
+    return True 
+    
+if __name__ == "__main__":
+    import doctest
+    print doctest.testmod(verbose=True)
